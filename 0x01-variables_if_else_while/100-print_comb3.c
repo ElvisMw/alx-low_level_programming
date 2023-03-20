a99022080324122b04e034bcc86a3aa088480a77
ElvisMw <elvismwanthi@gmail.com>
2023-03-20 08:45:40 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,13 +16,18 @@ int main(void)
 	{
 		for (j = i ; j < 10 ; j++)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-			putchar(',');
-			putchar(' ');
+			if (i < j && i != j)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				if (i + j != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
-	}
+	}	
 	putchar('\n');
 	return (0);
-
 }
