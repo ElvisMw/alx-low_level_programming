a2ccdd32517a4a8e10e5c63ce087323400b966d5
ElvisMw <elvismwanthi@gmail.com>
2023-04-11 11:00:23 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion -function that functions like the standart library puts().
+ * @s: This serves as a starting point for the function.
+ * to access and print the characters of the input string.
+ * Return: Always 0 upon success.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n);
+}
