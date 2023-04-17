cd59ec9ea314b2d4b2053c7b7be3e0914c65c690
ElvisMw <elvismwanthi@gmail.com>
2023-04-17 13:41:11 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,3 +1,9 @@
+/*
+ * *str_concat - pointer to combine two strings s1 + s2
+ * @s1: first input
+ * @ss2:second input
+ * Return: concated strings
+ */
 #include "main.h"
 #include <stdlib.h>
 
@@ -7,41 +13,39 @@ char *str_concat(char *s1, char *s2)
 	int i, ci;
 
 	if (s1 == NULL)
-		 s1 = "";
+		s1 = "";
 
 	if (s2 == NULL)
-		 s2 = "";
+		s2 = "";
 
 	i = ci = 0;
 
-	 while (s1[i] != '\0')
-		 i++;
+	while (s1[i] != '\0')
+		i++;
 
-	 while (s2[ci] != '\0')
-		 ci++;
+	while (s2[ci] != '\0')
+		ci++;
 
-	 conct = malloc(sizeof(char) * (i + ci + 1));
+	conct = malloc(sizeof(char) * (i + ci + 1));
 
-	 if (conct == NULL)
-		  return (NULL);
+	if (conct == NULL)
+		return (NULL);
 
-	  i = ci = 0;
+	i = ci = 0;
 
-	   while (s1[i] != '\0')
-	   {
-		   conct[i] = s1[i];
-		   i++;
-	   }
-	    while (s2[ci] != '\0')
-	    {
-		    conct[i] = s2[ci];
-		    i++, ci++;
-	    }
+	while (s1[i] != '\0')
+	{
+		conct[i] = s1[i];
+		i++;
+	}
+	while (s2[ci] != '\0')
+	{
+		conct[i] = s2[ci];
+		i++, ci++;
+	}
 
-	    conct[i] = '\0';
+	conct[i] = '\0';
 
-	    return (conct);
+	return (conct);
 
 }
-	
-
