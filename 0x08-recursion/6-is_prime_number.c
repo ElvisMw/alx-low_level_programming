a62ba2c95ce84ef4c268d05f3d058370ad50b2ae
ElvisMw <elvismwanthi@gmail.com>
2023-06-08 13:39:16 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,21 +10,11 @@ int is_prime_number(int n)
 	if (n <= 1)
 		return (0);
 
-	return (letty_prime(n, 2));
-}
-/**
- * letty_prime - Helper function to see if a number is prime recursively
- * @n: The number in study
- * @divisor: To check for divisibility
- * Return: 1 if the number is prime or 0 if not
- */
-int letty_prime(int n, int divisor)
-{
-	if (n == divisor)
-		return (1);
+	for (int i = 2; i * i  <= n; i++)
+	{
+		if (n % i == 0)
+			return (0);
+	}
 
-	if (n % divisor == 0)
-		return (0);
-
-	return (letty_prime(n, divisor + 1));
+	return (1);
 }
