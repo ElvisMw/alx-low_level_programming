2373f96fdbef9dc8119bce6d2cc3528e71e67b57
ElvisMw <elvismwanthi@gmail.com>
2023-06-09 08:45:07 -0700

    static lib c

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,1 +1,13 @@
-int _strlen(char *s)
+#include "main.h"
+/**
+ * _strlen_recursion - Returns the length of the string by recursion
+ * @s: string
+ * Return: String length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+
+	return (1 + _strlen_recursion(s + 1));
+}
