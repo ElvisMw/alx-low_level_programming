e7ef74b43b3ebff80716051458d596ad45a0ef7a
ElvisMw <elvismwanthi@gmail.com>
2023-06-10 01:59:38 -0700

    static libraries in c

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - allows for a block memory with known value.
+ * @s: starting address of memory.
+ * @b: wanted  value
+ * @n: for number of bytes thha need to be changed
+ *
+ * Return: will return changed array follwed by a new value for n bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
