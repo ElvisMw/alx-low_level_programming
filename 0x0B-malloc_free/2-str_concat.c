aa0cfb569709239f60a1aca150482e37be37ac45
ElvisMw <elvismwanthi@gmail.com>
2023-06-13 12:03:40 -0700

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,28 +9,35 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *concatenated;
-	int len1 = 0, len2 = 0, i, j;
+	char *conct;
+	int i, letty;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	while (s1[len1])
-		len1++;
-	while (s2[len2])
-		len2++;
+	i = ci = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s2[letty] != '\0')
+		letty++;
+	conct = malloc(sizeof(char) * (i + letty + 1));
 
-	concatenated = malloc((len1 + len2 + 1) * sizeof(char));
-	if (concatenated == NULL)
+	if (conct == NULL)
 		return (NULL);
+	i = letty = 0;
+	while (s1[i] != '\0')
+	{
+		conct[i] = s1[i];
+		i++;
+	}
 
-	for (i = 0; i < len1; i++)
-		concatenated[i] = s1[i];
-	for (j = 0; j < len2; j++)
-		concatenated[i + j] = s2[j];
-
-	concatenated[i + j] = '\0';
-	return (concatenated);
+	while (s2[letty] != '\0')
+	{
+		conct[i] = s2[letty];
+		i++, letty++;
+	}
+	conct[i] = '\0';
+	return (conct);
 }
