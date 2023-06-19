2d681ba8879ae1b508d42e7ddfcefc892fa19d71
ElvisMw <elvismwanthi@gmail.com>
2023-06-19 05:08:48 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure for  dog
+ * @name: Points dog's name
+ * @age: Dog's age
+ * @owner: Points dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
