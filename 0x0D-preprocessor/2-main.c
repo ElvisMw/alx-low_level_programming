ec3f805ae4fa71af12ad230004cdb4735dd5a881
ElvisMw <elvismwanthi@gmail.com>
2023-06-19 04:11:10 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - program starts here and take no arguements
+ * prints name of the source file
+ * Return: Always 0 upon success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
