1466863f79f90a1e2cebbfd0b0533155e8b581b5
ElvisMw <elvismwanthi@gmail.com>
2023-06-21 06:59:43 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - To print name
+ * @name: Person's name
+ * @f: Points to printing format
+ * Return: Null or nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
