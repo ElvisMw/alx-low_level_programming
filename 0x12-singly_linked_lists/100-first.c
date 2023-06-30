1aa5263d2bb90bdd4eca8b40d6a946ff806f15bd
ElvisMw <elvismwanthi@gmail.com>
2023-06-30 14:25:02 -0700

    100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_before_main - prints message before main.
+ */
+void print_before_main(void)
+{
+    printf("You're beat! and yet, you must allow,\n"
+           "I bore my house upon my back!\n");
+}
+
--- 0x12-singly_linked_lists/other_functions.c
+++ 0x12-singly_linked_lists/other_functions.c
@@ -3,11 +3,12 @@
 #include "lists.h"
 
 /**
- * add_node_end - Adds a new node at the end of a list_t list.
+ * add_node_end - Adds  new node at the end of  list_t list.
  * @head: Pointer to the head of the list.
- * @str: String to be added to the new node.
+ * @str: reorersents string  added to the new node.
  *
- * Return: The address of the new element, or NULL on failure.
+ * Return: upon success the address of the new element,
+ * otherwise NULL
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
@@ -49,7 +50,7 @@ list_t *add_node_end(list_t **head, const char *str)
  * print_list - Prints all the elements of a list_t list.
  * @h: Pointer to the head of the list.
  *
- * Return: The number of nodes in the list.
+ * Return: Numerical value for nodes in the list.
  */
 size_t print_list(const list_t *h)
 {
