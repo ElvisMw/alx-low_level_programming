593407fedc300cc977807430ee014d9f025f3716
ElvisMw <elvismwanthi@gmail.com>
2023-06-30 12:02:35 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,57 +1,25 @@
 #include "lists.h"
-#include <unistd.h>
-/**
- * _putchar - writes the character c to stdout
- * @c: Represents character to print
- *
- * Return: Upon success return 1,
- * upon error, -1 is returned, otherwise errno
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 
 /**
- * print_list - prints all the elements of a list_t list.
- * @h: Pointer to the head of the list.
+ * print_list - Prints all the elements of a list_t list.
+ * @h: The pointer to the head of the list.
  *
- * Return: numerical value for  nodes in the list.
+ * Return: The number of nodes.
  */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t nodes_count = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		if (h->str == NULL)
-		{
-
-			_putchar('[');
-			_putchar('0');
-			_putchar(']');
-			_putchar(' ');
-			_putchar('(');
-			_putchar('n');
-			_putchar('i');
-			_putchar('l');
-			_putchar(')');
-			_putchar('\n');
-		}
-
+		if (h->str)
+			printf("[%u] %s\n", h->len, h->str);
 		else
-		{
-			size_t letty;
+			printf("[0] (nil)\n");
 
-			for (letty = 0; h->str[letty] != '\0'; letty++)
-				_putchar(h->str[letty]);
-
-			_putchar('\n');
-		}
-
+		nodes_count++;
 		h = h->next;
-		count++;
 	}
 
-	return (count);
+	return (nodes_count);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,24 +1,27 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 /**
- * struct list_s - This is singly linked list
- * @str: Reprsents string - (malloc'ed string)
- * @len: Represents length of the string
- * @next: Represents points to next node
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: Represents length of string
+ * @next: Preperesnets points to next node
  *
  * Description: singly linked list node structure
  */
-
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
+int _putchar(char c);
 size_t print_list(const list_t *h);
 
 #endif
