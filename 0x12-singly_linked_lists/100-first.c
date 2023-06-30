40a0eb2efd05b0c1f81f7f58dd1df434e5a6ac48
ElvisMw <elvismwanthi@gmail.com>
2023-06-30 14:38:46 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,9 +4,9 @@
 /**
  * print_before_main - prints message before main.
  */
+void print_before_main(void) __attribute__((constructor));
 void print_before_main(void)
 {
-    printf("You're beat! and yet, you must allow,\n"
-           "I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
-
