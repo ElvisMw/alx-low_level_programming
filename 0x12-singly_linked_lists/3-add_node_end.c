347631e639c70b26dea987a975d8e4c8c04cb36d
ElvisMw <elvismwanthi@gmail.com>
2023-06-30 13:45:58 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,50 +9,33 @@
  * Return: Upon sucess, address of the new element
  * otherwise, NULL.
  */
-list_t *add_node_end(list_t **head, const char *str)
+list_t* add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node, *last_node;
+	list_t * new;
+	list_t *temp = *head;
+	unsigned int len = 0;
 
-	/* Check whether input string is NULL */
-	if (str == NULL)
+	while (str[len])
+		len++;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
 		return (NULL);
 
-	/*assisgn  memory new node */
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
+	new->str = strdup(str);
+	new->len = len;
+	new->next = NULL;
 
-	/* copy string */
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
-	/* computes string's length */
-	new_node->len = strlen(str);
-
-	/* Set the new node's next pointer to NULL-set as final node */
-	new_node->next = NULL;
-
-	/* If the list is empty, set the new node as the head */
 	if (*head == NULL)
 	{
-		*head = new_node;
-	}
-	else
-	{
-		/* Traverse the list to find the last node */
-		last_node = *head;
-		while (last_node->next != NULL)
-		{
-			last_node = last_node->next;
-		}
-
-		/* Assign the new node as the next node of the last node */
-		last_node->next = new_node;
+		*head = new;
+		return (new);
 	}
 
-	return (new_node);
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = new;
+
+	return (new);
 }
