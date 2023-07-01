04c7e12aac3e36c26ca956aefba1a2631d9b187b
ElvisMw <elvismwanthi@gmail.com>
2023-07-01 14:55:03 -0700

    101-hello_holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,16 @@
 section .data
-    hello db "Hello, Holberton",0
-    format db "%s", 10, 0
+    hello db "Hello, Holberton",0        ; Define the 'hello' string via null-termination
+
+    format db "%s", 10, 0                ; Defines format specifier
 
 section .text
-    extern printf
+    extern printf                       ; Declare the external printf function
 
     global main
 main:
-    push rbp
-    mov rdi, format
-    mov rsi, hello
-    call printf
-    pop rbp
-    ret
+    push rbp                            ; Save the base pointer
+    mov rdi, format                     ; Load the address of the format specifier into rdi
+    mov rsi, hello                      ; Load the address of the 'hello' string into rsi
+    call printf                         ; Call  printf function to print the string
+    pop rbp                             ; Restore base pointer
+    ret                                 ; Return from main function
