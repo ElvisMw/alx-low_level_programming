fab1230da3e6dea8556d4dfdaab1c5a9f6253841
ElvisMw <elvismwanthi@gmail.com>
2023-07-10 15:40:25 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,8 @@ int create_file(const char *filename, char *text_content)
 			text_len++;
 	}
 
-	emw_file_description = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	emw_file_description = open(filename, O_WRONLY |
+			O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (emw_file_description == -1)
 		return (-1);
 
