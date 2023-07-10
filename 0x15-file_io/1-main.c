431cd0f13b4a6ef4970ed790b1612a9561366ee7
ElvisMw <elvismwanthi@gmail.com>
2023-07-10 15:33:10 -0700

    1-create_file.c

--- 0x15-file_io/1-main.c
+++ 0x15-file_io/1-main.c
@@ -9,16 +9,16 @@
  */
 int main(int ac, char **av)
 {
-    int res;
+	int res;
 
-    if (ac != 3)
-    {
-        fprintf(stderr, "Usage: %s filename text\n", av[0]);
-        exit(1);
-    }
+	if (ac != 3)
+	{
+		fprintf(stderr, "Usage: %s filename text\n", av[0]);
+		exit(1);
+	}
 
-    res = create_file(av[1], av[2]);
-    printf("-> %i)\n", res);
+	res = create_file(av[1], av[2]);
+	printf("-> %i)\n", res);
 
-    return (0);
+	return (0);
 }
