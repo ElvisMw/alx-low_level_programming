700a0033909b85d8a765eaa1c9a01f3a0a1987a9
ElvisMw <elvismwanthi@gmail.com>
2023-07-11 12:48:02 -0700

    100-elf_header.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,6 +4,7 @@
 #define BUFFER_SIZE 1024
 #define PERMISSIONS 0664
 
+#include <elf.h>
 #include <unistd.h>
 
 int _putchar(char c);
@@ -12,5 +13,16 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int cp_file(const char *file_from, const char *file_to);
 void print_elf_header(char *filename);
+void print_error(char *msg);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_abiversion(unsigned char *e_ident);
+void print_type(unsigned int e_type);
+void print_entry(unsigned long int e_entry);
+void read_elf_header(int fd, Elf64_Ehdr *e_hdr);
+void elf_header(char *filename);
 
 #endif
